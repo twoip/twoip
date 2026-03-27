b34842acb5862a03266ce5517fdea22df4cf0144
teplostanski <igor@teplostanski.me>
2026-03-27 03:08:30 +0300

    feat: refactoring and add new methods

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,107 @@
 type Lang = 'en' | 'ru'
 
-type QueryParams = {
+interface TwoIP {
   token: string
+}
+
+interface ApiUrl {
+  token: string
+  service: keyof typeof endpoints
+  target?: string
+  lang?: Lang
+  page?: number
+}
+
+type IpInfo = {
+  ip?: string
   lang?: Lang
 }
 
-type TwoIP = QueryParams
-
-type BuildApiUrl = QueryParams & {
-  service?: string
-  address?: string
+type Email = {
+  email: string
 }
 
-function buildApiUrl({ service, address, token, lang }: BuildApiUrl): string {
+type AbuseReportByIp = { ip: string }
+type AbuseReportByDomain = { domain: string }
+type AbuseReports = AbuseReportByIp | AbuseReportByDomain
+
+const endpoints = {
+  info: '',
+  whois: 'whois',
+  abuses: 'abuses',
+  domains: 'domains',
+  email: 'email',
+  asn: 'asn',
+} as const
+
+function buildApiUrl({ service, target, token, lang, page }: ApiUrl): string {
   const baseUrl = 'https://api.2ip.io'
-  const path = [service, address].filter(Boolean).join('/')
+  const path = [endpoints[service], target].filter(Boolean).join('/')
+  console.log(target, path)
+
   const url = new URL(`${baseUrl}/${path}`)
 
   url.searchParams.set('token', token)
   if (lang) {
     url.searchParams.set('lang', lang)
   }
+  if (page) {
+    url.searchParams.set('page', String(page))
+  }
 
   return url.toString()
 }
 
-export function twoip({ token, lang }: TwoIP) {
-  const ipInfo = async (ip?: string) => {
-    const url = buildApiUrl({ address: ip, token, lang })
+async function fetchApi(url: string) {
+  console.log(url)
 
-    console.log(url)
+  try {
+    const response = await fetch(url)
 
-    try {
-      const response = await fetch(url)
-
-      if (!response.ok) {
-        throw new Error(`HTTP status: ${response.status} - ${response.statusText}`)
-      }
-
-      const data = await response.json()
-      console.log('Response API:', data)
-
-      return data
-    } catch (error) {
-      console.log(error)
+    if (!response.ok) {
+      throw new Error(
+        `HTTP status: ${response.status} - ${response.statusText}`,
+      )
     }
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+
+export function twoip({ token }: TwoIP) {
+  const getIpInfo = async ({ ip, lang }: IpInfo = {}) => {
+    const url = buildApiUrl({ service: 'info', target: ip, token, lang })
+    return await fetchApi(url)
   }
 
-  return { ipInfo }
+  const checkEmail = async ({ email }: Email) => {
+    const url = buildApiUrl({ service: 'email', target: email, token })
+    return await fetchApi(url)
+  }
+
+  const getAbuseReports = async (params : AbuseReports) => {
+    // TODO: затащить zod. если params.ip валидировать как ip, если params.domain валидировать как домен
+    const address = 'ip' in params ? params.ip : params.domain
+    const url = buildApiUrl({ service: 'abuses', target: address, token })
+    return await fetchApi(url)
+  }
+
+  //getDomainsByIp(ip, page?)
+  //getAsnInfo(asnId, lang?)
+  //getWhoisInfo(ip)
+
+  return { getIpInfo, checkEmail, getAbuseReports }
 }
 
 if (!process.env.TSDOWN_TOKEN) {
   throw new Error('TSDOWN_TOKEN not found in .env')
 }
 
-const { ipInfo } = twoip({ token: process.env.TSDOWN_TOKEN, lang: 'ru' })
+const { checkEmail, getIpInfo, getAbuseReports } = twoip({ token: process.env.TSDOWN_TOKEN })
 
-ipInfo()
-ipInfo('138.249.149.55')
+//console.log(await checkEmail({ email: 'dev@2ip.io' }))
+//console.log(await getIpInfo({ ip: '138.249.149.55', lang: 'ru' }))
+console.log(await getAbuseReports({ domain: 'facebook.com' }))
